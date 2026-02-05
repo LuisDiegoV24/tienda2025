dc5679265e93eccee9fe8a78d0363b611284c26b
Luis Diego <luisdiego2411@gmail.com>
2026-02-04 23:15:44 -0600

    Cambios semana 4 (sin credenciales)

--- src/main/java/com/tienda/domain/Categoria.java
+++ src/main/java/com/tienda/domain/Categoria.java
@@ -14,8 +14,11 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
 
-
+@Getter
+@Setter
 @Entity
 @Table(name = "categoria")
 public class Categoria implements Serializable {
--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -22,6 +22,9 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository categoriaRepository;
     
+    @Autowired
+    private FirebaseStorageService firebaseStorageService;
+    
     @Transactional(readOnly=true)
     public List<Categoria> getCategorias(boolean activo) {
         if (activo) {
@@ -30,5 +33,35 @@ public class CategoriaService {
         return categoriaRepository.findAll();
     }
     
+    @Transactional(readOnly=true)
+    public Optional<Categoria> getCategoria(Integer idCategoria) {
+        return categoriaRepository.findById(idCategoria);
     }
     
+    @Transactional
+    public void save(Categoria categoria, MultipartFile imagenFile) {
+        categoria = categoriaRepository.save(categoria);
+        if (imagenFile.isEmpty())
+            return;
+        try {
+            String rutaImagen = firebaseStorageService.uploadImage(imagenFile,
+                    "categoria",
+                    categoria.getIdCategoria());
+            categoria.setRutaImagen(rutaImagen);
+            categoriaRepository.save(categoria);
+        } catch (IOException e) {
+        }
+    }
+    
+    @Transactional
+    public void delete(Integer idCategoria) {
+        if (!categoriaRepository.existsById(idCategoria)) {
+            throw new IllegalArgumentException("La categoria con ID " + idCategoria + " no existe");
+        }
+        try {
+            categoriaRepository.deleteById(idCategoria);
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalStateException("No se puede eliminar la categoria, tiene datos asociados", e);
+        }
+    }
+}
